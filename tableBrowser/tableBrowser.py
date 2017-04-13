afcd8d851a14ca7b551adade16b01d4c30050063
Julius Gonsior <julius@gaenseri.ch>
2017-04-13 12:14:27 +0200

    added database functions

--- tableBrowser/tableBrowser.py
+++ tableBrowser/tableBrowser.py
@@ -19,3 +19,17 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
