23d821ea04b55307dd5045a15e7b963538d09683
Julius Gonsior <julius@gaenseri.ch>
2017-04-13 18:43:33 +0200

    added ability to show one specific table

--- tableBrowser/tableBrowser.py
+++ tableBrowser/tableBrowser.py
@@ -1,24 +1,29 @@
 import os
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import ujson
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+from pprint import pprint
 
-app = Flask(__name__) # create the application instance :)
-app.config.from_object(__name__) # load config from this file 
+app = Flask(__name__)  # create the application instance :)
+app.config.from_object(__name__)  # load config from this file
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'data.db')
-#    SECRET_KEY='development key',
-#    USERNAME='admin',
-#    PASSWORD='default'
+    #    SECRET_KEY='development key',
+    #    USERNAME='admin',
+    #    PASSWORD='default'
 ))
 app.config.from_envvar('TABLE_BROWSER_SETTINGS', silent=True)
+
 
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -27,6 +32,7 @@ def get_db():
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
+
 
 @app.teardown_appcontext
 def close_db(error):
@@ -42,7 +48,10 @@ def showTables():
     entries = c.fetchall()
     return render_template('show_tables.html', entries=entries)
 
+
 @app.route('/show/<int:tableId>')
 def showTable(tableId):
-    c = get_db().execute('SELECT * FROM tables WHERE id="?"', (tableId,))
-    return c
+    c = get_db().execute('SELECT * FROM tables WHERE id=(?)', (tableId,))
+    meta = c.fetchone()
+    table=ujson.loads(meta['cells'])
+    return render_template('show_table.html', meta=meta, table=table)
