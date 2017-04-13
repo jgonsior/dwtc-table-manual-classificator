570a857993bd491b459bf3b1b031e9d12edcf624
Julius Gonsior <julius@gaenseri.ch>
2017-04-13 19:47:59 +0200

    moved preparation of database into flask

--- tableBrowser/tableBrowser.py
+++ tableBrowser/tableBrowser.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3
 import ujson
+import click
+import gzip
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
 from flask_bootstrap import Bootstrap
@@ -10,46 +12,58 @@ from pprint import pprint
 app = Flask(__name__)  # create the application instance :)
 Bootstrap(app)
 app.config.from_object(__name__)  # load config from this file
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'data.db')
-    #    SECRET_KEY='development key',
-    #    USERNAME='admin',
-    #    PASSWORD='default'
-))
 app.config.from_envvar('TABLE_BROWSER_SETTINGS', silent=True)
 
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+class Table(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pageTitle = db.Column(db.Text)
+    url = db.Column(db.Text)
+    title = db.Column(db.Text)
+    tableType = db.Column(db.Text)
+    cells = db.Column(db.Text)
+
+    def __init__(self, pageTitle, url, title, tableType, cells):
+        self.pageTitle = pageTitle
+        self.url = url
+        self.title = title
+        self.tableType = tableType
+        self.cells = cells
+
+    def __repr__(self):
+        return '<Table %r>' % self.id
 
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+@app.cli.command('initDb')
+@click.argument('sourceFile')
+def initdbCommand(sourcefile):
+    """Initializes the database."""
+    db.create_all()
 
+    # first unzip file
+    with gzip.open(sourcefile, "rb") as file:
+        # then iterate over it line by line
+        for line in file:
+            # and finally save each entry into a new database
+            rawData = ujson.loads(line)
+            table = Table(rawData['pageTitle'], rawData['url'],
+                          rawData['title'], rawData['tableType'],
+                          ujson.dumps(rawData['relation']))
+            db.session.add(table)
+        #could be optimized, but only if needed to :)
+        db.session.commit()
 
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+    print('Initialized the database')
 
 
 @app.route('/')
 def showTables():
-    db = get_db()
-    c = db.execute('SELECT * FROM tables LIMIT 100')
-    entries = c.fetchall()
+    entries = Table.query.limit(100).all()
     return render_template('show_tables.html', entries=entries)
 
 
@@ -57,5 +71,5 @@ def showTables():
 def showTable(tableId):
     c = get_db().execute('SELECT * FROM tables WHERE id=(?)', (tableId,))
     meta = c.fetchone()
-    table=ujson.loads(meta['cells'])
+    table = ujson.loads(meta['cells'])
     return render_template('show_table.html', meta=meta, table=table)
