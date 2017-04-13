e0dcfc975188da28bb7dfdfb52f14e7bb534a802
Julius Gonsior <julius@gaenseri.ch>
2017-04-13 12:04:37 +0200

    initialized flask structure

---
+++ tableBrowser/__init__.py
@@ -0,0 +1,1 @@
+from .tableBrowser import app
---
+++ tableBrowser/tableBrowser.py
@@ -0,0 +1,21 @@
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) # load config from this file 
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'data.db')
+#    SECRET_KEY='development key',
+#    USERNAME='admin',
+#    PASSWORD='default'
+))
+app.config.from_envvar('TABLE_BROWSER_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
