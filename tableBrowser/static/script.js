ad38116a99c1a6af778cd9060154110da5b18ffa
Julius Gonsior <julius@gaenseri.ch>
2017-04-14 14:42:07 +0200

    better hover effect

--- tableBrowser/static/script.js
+++ tableBrowser/static/script.js
@@ -1,16 +1,17 @@
 function toggleClasses(className) {
-  $('table').removeClass('ENTITY');
-  $('table').removeClass('RELATION');
-  $('table').removeClass('MATRIX');
-  $('table').removeClass('OTHER');
-  $('table').addClass(className);
+    $('table').removeClass('ENTITY');
+    $('table').removeClass('RELATION');
+    $('table').removeClass('MATRIX');
+    $('table').removeClass('OTHER');
+    $('table').addClass(className);
 }
- $(document).ready(function() {
-     $('#entBtn').hover(function() {
-         toggleClasses('ENTITY');
-         originalClass = $('h2').attr('type');
-     }, function() {
-         $('table').removeClass('ENTITY');
-         $('table').addClass(originalClass);
-     });
- });
+$(document).ready(function() {
+    var originalClass
+    $('#classificationButtons button').hover(function() {
+        $('table').addClass($(this).text() + '-hover');
+        originalClass = $('h2').attr('type');
+    }, function() {
+        $('table').removeClass($(this).text() + '-hover');
+        $('table').addClass(originalClass);
+    });
+});
