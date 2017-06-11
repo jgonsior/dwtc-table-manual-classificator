3ab01e9ffa0daa333f59ed6ac2e7e39515d758f6
Julius Gonsior <julius@gaenseri.ch>
2017-06-11 14:07:34 +0200

    Added some statistics

--- featureClassificator/src/main/java/Main.java
+++ featureClassificator/src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.sql.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("hallo welt");
+		//1. load database tables into jsoup objects
+		Connection connection = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:dwtcTableManualClassificator/data.db");
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT count(originalTableType), originalTableType FROM `table` GROUP BY originalTableType");
+			while(resultSet.next()) {
+				System.out.println("Original table type: " + resultSet.getString(2) + ": " + resultSet.getInt(1));
+			}
+			
+			System.out.println("\n----------------------------------------\n");
+			
+			resultSet = statement.executeQuery("SELECT count(newTableType), newTableType FROM `table` GROUP BY newTableType");
+			while(resultSet.next()) {
+				System.out.println("New table type: " + resultSet.getString(2) + ": " + resultSet.getInt(1));
+			}
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
